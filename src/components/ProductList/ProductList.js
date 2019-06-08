1d2b8b37cd6b16e14b3cbd7825245573dc1a8461
Tomasz Wylężek <tomwylezek@gmail.com>
2019-06-08 09:36:15 +0200

    changes

---
+++ src/components/ProductList/ProductList.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const ProductList = () => (
+  <ul>
+    <li>Apple</li>
+    <li>Coffee</li>
+  </ul>
+);
---
+++ src/containers/Root.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { ProductList } from "../components/ProductList/ProductList";
+
+export const Root = () => <ProductList />;
